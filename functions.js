4cc819ec26e312666431a91e6a962628e8c971ed
Prashant <kv.prshnt@gmail.com>
2013-02-18 10:24:14 +0530

    Template and css changes, Added call for action buttons

--- functions.js
+++ functions.js
@@ -2,6 +2,107 @@ if (Meteor.isClient) {
 
   $('#navbar').affix();
 
+  var enableRegistrations = false; // Do you want to enable registrations?
+  var eventLink = "http://doattend.com"; // Your event link goes here
+
+  var partnersDir = "/partners/";
+  var email = "kv.prshnt@gmail.com";
+  var curator = "Prashant";
+
+  // Default definitions. No need to change unless css is changed
+  var registerEnableClass = "btn-warning";
+  var registerDisableClass = "btn-disable";
+
+  Template.jumbotron.title = function() {
+    return "Devthon";
+  }
+
+  Template.jumbotron.edition = function() {
+    return "1st";
+  }
+
+  Template.jumbotron.tagline = function() {
+    return "Launchpad for ideas, code contributions and discussions";
+  }
+
+  Template.jumbotron.requestInviteText = function() {
+    return "Request Invite";
+  }
+
+  Template.jumbotron.requestInviteLink = function() {
+    return "#request";
+  }
+
+  Template.jumbotron.ticketLink = function() {
+    var ticketLink = enableRegistrations ? eventLink : "";
+    return ticketLink;
+  }
+
+  Template.jumbotron.ticketText = function() {
+    var ticketText = enableRegistrations ? "Already invited?" : "Invitations and Hack proposals will open soon";
+    return ticketText;
+  }
+
+  Template.jumbotron.buttonClass = function() {
+    var buttonClass = enableRegistrations ? registerEnableClass : registerDisableClass;
+    return buttonClass;
+  }
+
+  Template.jumbotron.isRegistrationOpen = enableRegistrations;
+
+  Template.jumbotron.price = function() {
+    return "Rs. 1000";
+  }
+
+  Template.dates.when = function() {
+    return "March 30-31";
+  }
+
+  Template.about.content = function() {
+    return "Devthon is a launchpad for ideas, code contributions and discussions. \
+            Meet curious and tinkering minds and spend a weekend getting a high on new ideas, code and hardware. \
+            This is a developer marathon running on caffeine and <a href='http://en.wikipedia.org/wiki/Vada_pav'>vada pav</a>.";
+  }
+
+  Template.about.contact = function() {
+    return "Mail <a href='mailto:" + email + "'>" + curator + "</a> if you are interested in participating.";
+  }
+
+  Template.footer.copyright = function() {
+    return "Copyright © 2013 Devthon.";
+  }
+
+  Template.footer.author = function () {
+    return "Hacked by <a href='http://twitter.com/prashantkv'>@prashantkv</a>";
+  }
+  
+  Template.navigation.navs = function() {
+    return [
+            { "text": "Hacks", "link": "http://www.google.com/moderator/#16/e=2052b0" }, 
+	    { "text": "About", "link": "#about" }
+	   ]
+  }
+
+  Template.discussions.tracks = function() {
+    return [
+       { 
+           "name" : "Web Hacks", 
+           "description" : "Hack ideas and discussions related to frameworks like NodeJS, MeteorJS, Python frameworks (Flask, Django), etc",
+           "link" : "http://www.google.com/moderator/#15/e=2052b0&t=2052b0.43"
+       },
+       {
+           "name" : "Mobile Hacks",
+           "description" : "Hack ideas and discussions related to all things Mobile like PhoneGap, Sencha, Android, iOS",
+           "link": "http://www.google.com/moderator/#15/e=2052b0&t=2052b0.42"
+       },
+       {
+           "name" : "Hardware hacks",
+           "description" : "Ideas and discussions related to all things hardware like Leap Motion/Arduino/Raspberry",
+           "link" : "http://www.google.com/moderator/#15/e=2052b0&t=2052b0.41"
+       }
+     ]
+  }
+
   Template.setup.created = function() {
     if (!(window._gaq != null)) {
       window._gaq = [];
@@ -20,21 +121,47 @@ if (Meteor.isClient) {
     }
   };
 
-  Template.hello.greeting = function () {
-    return "Welcome to devthon.";
-  };
+  Template.twitter.created = function() {
+    return !function(d,s,id) {
+             var js,fjs = d.getElementsByTagName(s)[0];
+             if(!d.getElementById(id)){
+               js=d.createElement(s);
+               js.id=id;
+               js.src="//platform.twitter.com/widgets.js";
+               fjs.parentNode.insertBefore(js,fjs);
+             }
+     }(document,"script","twitter-wjs");
+  }
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  Template.facebook.created = function() {
+    return (function(d, s, id) {
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) return;
+             js = d.createElement(s); js.id = id;
+             js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=450411951699240";
+             fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
+  // In your server code: define a method that the client can call
+    Meteor.methods({
+      sendEmail: function (to, from, subject, text) {
+        // Let other method calls from the same client start running,
+        // without waiting for the email sending to complete.
+        this.unblock();
+
+        Email.send({
+          to: to,
+          from: from,
+          subject: subject,
+          text: text
+        });
+      }
+    });
   });
 }
